06ff806168fbe29cca6f21abad1c239fba3fe659
Syed M. Sawaid <34231494+SyedMSawaid@users.noreply.github.com>
2021-04-13 17:50:59 +0500

    Added Home, Sign up and Sign in Components
    Added Home, Sign up and Sign in components along with Navbar as well. Set up a routes for Signup, Sign in and Home.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,16 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseDetailComponent } from './courses/course-detail/course-detail.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseDetailComponent,
+    NavbarComponent,
+    LoginComponent,
+    SignupComponent,
+    HomeComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CollapseModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
